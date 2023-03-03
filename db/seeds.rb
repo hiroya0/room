0cc947d628645259f5b524c75221206628b9b586
hiroya0 <2000shigehiro0401@au.com>
2023-03-03 21:58:59 +0900

    課題提出2回目

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Room.create!(
+   name: 'テスト',
+   introduction: 'テスト',
+   address: '札幌',
+   room_image: File.open('https://rails-02-sample.herokuapp.com/assets/common/default-avatar-7a6cbfd7993e89f24bfc888f4a035a83c6f1428b8bdc47eed9095f2799a40153.png')
+   price: '1234'
+)
